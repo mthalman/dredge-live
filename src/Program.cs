9abda57e226650036721ec4314a291de442d56e5
Matt Thalman <mthalman@microsoft.com>
2023-02-21 19:24:53 -0600

    Define general layout of site

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,13 @@
-using Valleysoft.Dredge.Live;
+using BlazorPro.BlazorSize;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Valleysoft.Dredge.Live;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMediaQueryService();
 
 await builder.Build().RunAsync();
